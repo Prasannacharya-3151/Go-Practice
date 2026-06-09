dc8d9615add1261f4b58e327e2c9eb7892a9b0c0
Prasannacharya-3151 <prasannacharya428@gmail.com>
2026-06-10 00:29:04 +0530

    .

--- writing_files.go
+++ writing_files.go
@@ -1,3 +1,23 @@
-package main() {
-	
+package main
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	//Writing files means storing data from your Go program into a file on your computer.
+	data := "Hello Prasanna"
+
+	err := os.WriteFile(
+		"test.txt",
+		[]byte(data),
+		0644,
+	)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return 
+	}
+
+	fmt.Println("File Written successfully")
 }
