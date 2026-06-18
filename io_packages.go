90feb18eb1862ebd86337a301c695056d9680756
Prasannacharya-3151 <prasannacharya428@gmail.com>
2026-06-18 15:47:16 +0530

    .

---
+++ io_packages.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func main(){
+	//The io package provides interfaces and helper functions for reading and writing data. The main interfaces are io.Reader and io.Writer.
+	//bufio
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter Name:")
+	name, _ := reader.ReadString('\n')
+	fmt.Println(name)
+
+	//io
+	strReader := strings.NewReader(
+		"Hello Go",
+	)
+
+	data, _ := io.ReadAll(strReader) //read entire content
+	fmt.Println(string(data))
+}
