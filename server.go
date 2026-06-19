49bc548ef0bc1605ca83a11a2dbe4c7edb68ee8b
Prasannacharya-3151 <prasannacharya428@gmail.com>
2026-06-19 23:12:15 +0530

    .

--- server.go
+++ server.go
@@ -10,9 +10,9 @@ func main() {
 	http.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request){
 		fmt.Fprintln(resp, "Hello server!")
 	})
-	const serverAddr string = "127.0.0.1:3000" //address:127.0.0.1 and gate number 3000
+	const serverAddr string= "127.0.0.1:8080" //address:127.0.0.1 and gate number 3000
 
-	fmt.Println("Server listening on port 3000")
+	fmt.Println("Server listening on port:", serverAddr)
 	err := http.ListenAndServe(serverAddr, nil)
 	if err !=nil {
 		log.Fatalln("error starting server", err)
